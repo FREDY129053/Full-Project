c366b653e80ea52e063fd1be5b77977818bf9389
mnhtrk <ilya.ivan0v@mail.ru>
2023-04-25 22:21:30 +1000

    filter test

--- DNS_Mentor/project/views.py
+++ DNS_Mentor/project/views.py
@@ -6,8 +6,6 @@ from .forms import MentorForm
 from .models import Mentor
 from django.http import HttpResponse
 from django.db.models import Q
-from django.db.models.query import QuerySet
-
 
 # ментор
 def mentor(request):
@@ -17,23 +15,26 @@ def mentor(request):
 # каталог
 def catalog(request):
     search_query = request.GET.get('q', '')
-    if search_query:
-        mentors = Mentor.objects.filter(Q(mentor_surname__icontains=search_query) |
-                                        Q(mentor_name__icontains=search_query) |
-                                        Q(sphere__icontains=search_query))
+    filters = request.GET.get('select', '')
+    if filters:
+        if search_query:
+            mentors = Mentor.objects.filter(Q(mentor_surname__icontains=search_query) |
+                                            Q(mentor_name__icontains=search_query) |
+                                            Q(sphere__icontains=search_query) |
+                                            Q(sphere__icontains=filters))
+        else:
+            mentors = Mentor.objects.filter(Q(sphere__icontains=filters))
     else:
-        mentors = Mentor.objects.all()
+        if search_query:
+            mentors = Mentor.objects.filter(Q(mentor_surname__icontains=search_query) |
+                                            Q(mentor_name__icontains=search_query) |
+                                            Q(sphere__icontains=search_query))
+        else:
+            mentors = Mentor.objects.all()
 
     print(mentors)
     context = {'mentors': mentors}
     return render(request, 'project/catalog.html', context)
-
-
-def postsearch(request):
-    search = request.POST.get("search", "ALina")
-    # return HttpResponse(f"<h2>{search}</h2>")
-    print(search)
-
 
 # домашняя страница
 def home(request):
